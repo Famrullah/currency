f454c819bb4dc9246994b2b33805235a08cb59cb
Faisal <faisal@jojonomic.com>
2020-01-14 00:16:35 +0700

    delete

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -54,18 +54,3 @@ export const optionCurrency = [
     isDisabled: false
   }
 ];
-
-// idr,gdp,eur,sgd
-
-export const initialCurrentData = [
-  {
-    currencyName:'Canadian Dollars',
-    currencyValue:'CAD',
-    rate:1.3071860067
-  },
-  {
-    currencyName:'Canadian Dollars',
-    currencyValue:'CAD',
-    rate:1.3071860067
-  },
-]
